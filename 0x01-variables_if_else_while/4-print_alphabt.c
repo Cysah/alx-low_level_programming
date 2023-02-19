350d7bcfdd5670013ee845394d253980a142300b
Cysah <sisathembela@gmail.com>
2023-02-19 14:01:31 -0800

    Print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets in lower case
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+
+	if(alp == 'q' || alp == 'e')
+		continue;
+	putchar(alp);
+	
+	}		
+
+	putchar('\n');
+	return (0);
+
+}
+
