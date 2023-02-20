ed56b3d91d6187b96dc2e1942f6cd634c3fbbb91
Cysah <sisathembela@gmail.com>
2023-02-19 18:34:10 -0800

    Print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print combinations of sinlge digit
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n)
+		if (n != '9')
+		{
+			putchar(',');
+			putchar (' ');
+		}
+		        ++n:;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
