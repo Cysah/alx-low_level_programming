f704bb7808178da52b43f887497e2114eaee41b1
Cysah <sisathembela@gmail.com>
2023-02-21 16:20:41 -0800

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - A program that print alphabet in lower case.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'Z'
+	{
+	        _putchar(alp);
+		alp++
+	}
+	_putcharr('\n');
+	return (0);
+}
