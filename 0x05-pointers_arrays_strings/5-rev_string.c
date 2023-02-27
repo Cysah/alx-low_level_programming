9b0ed59ee5aca41a69ffa583b28b1f2d3d0e8939
Cysah <sisathembela@gmail.com>
2023-02-27 13:11:15 -0800

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,9 +4,24 @@
  * rev_string - a function that reverse a string
  *
  * @s: string to be reversed
+ * Return: string in rervese
  */
 
 void rev_string(char *s)
 {
-	int a. b;
+	char rev = s[0];
 
+	int counter = 0;
+
+	int i;
+
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
