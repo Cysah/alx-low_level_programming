fc5b9b0667ea3e48c19510725158973cea6db83f
Cysah <sisathembela@gmail.com>
2023-03-02 05:59:49 -0800

    Strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,21 +11,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int cpy_str = 0;
-	int char_str = 0;
+	int i;
 
-	while (src[cpy_str++])
-	{
-		char_str++;
-	}
-	for (cpy_str = 0; src[cpy_str] && cpy_str < n; cpy_str++)
-	{
-		dest[cpy_str] = src[char_str];
-
-		for (index = char_str; cpy_str < n; cpy_str++)
-		{
-			dest[cpy_str] = '\0';
-		}
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; n > i; i++)
+		dest[i] = '\0';
 	return (dest);
 }
